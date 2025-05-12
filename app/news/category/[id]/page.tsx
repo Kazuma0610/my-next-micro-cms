f720749b33c7e670a367290645ee703ae2282cc9
KazumaOchiai <kazuma06101209@gmail.com>
2025-05-12 22:40:02 +0900

    ページネーション制作

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
 import Category from "@/app/_components/Category";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 type Props = {
     params: {
@@ -15,6 +16,7 @@ export default async function Page( { params }: Props ) {
 
 
     const { contents: news } = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
         filters: `category[equals]${category.id}`,
     });
 
