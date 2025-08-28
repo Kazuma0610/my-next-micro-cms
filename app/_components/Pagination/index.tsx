313c6f6e7378f49ef1d3ef51c4e14003c76132dc
KazumaOchiai <kazuma06101209@gmail.com>
2025-08-28 17:56:16 +0900

    ページネーション修正

--- app/_components/Pagination/index.tsx
+++ app/_components/Pagination/index.tsx
@@ -3,28 +3,31 @@ import Link from "next/link";
 import styles from "./index.module.css";
 
 type Props = {
-    totalCount: number;
+  totalCount: number;
+  current?: number;
 };
 
-export default function Pagination({ totalCount }: Props) {
-    const pages = Array.from(
-        { length: Math.ceil(totalCount / NEWS_LIST_LIMIT) },
-        (_, i) => i + 1
-)
+export default function Pagination({ totalCount, current = 1 }: Props) {
+  const pages = Array.from(
+    { length: Math.ceil(totalCount / NEWS_LIST_LIMIT) },
+    (_, i) => i + 1
+  );
 
-
-    return (
-        <nav>
-            <ul className={styles.container}>
-                {pages.map((p) =>(
-                    <li className={styles.list} key={p}>
-                        <Link href={`/news/p/${p}`} className={styles.item}>
-                        {p}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className={styles.container}>
+        {pages.map((p) => (
+          <li className={styles.list} key={p}>
+            {current !== p ? (
+              <Link href={`/news/p/${p}`} className={styles.item}>
+                {p}
+              </Link>
+            ) : (
+              <span className={`${styles.item} ${styles.current}`}>{p}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
-
--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -31,7 +31,7 @@ export default async function Page({ params }: Props) {
     <>
       <Breadcrumbs />
       <NewsList news={news} />
-      <Pagination totalCount={totalCount} />
+      <Pagination totalCount={totalCount} current={current} />
     </>
   );
 }
