ddcd073a184362b7567f7fd4c8e674b46568e993
KazumaOchiai <kazuma06101209@gmail.com>
2025-08-29 11:21:16 +0900

    検索フォーム設置

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,7 @@
 import { getNewsList } from "../_libs/microcms";
 import NewsList from "../_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
+import SearchField from "@/app/_components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 import Breadcrumbs from "@/app/_components/Breadcrumbs";
 
@@ -12,6 +13,7 @@ export default async function Page() {
   return (
     <>
       <Breadcrumbs />
+      <SearchField />
       <NewsList news={news} />
       <Pagination totalCount={totalCount} />
     </>
---
+++ app/news/search/page.tsx
@@ -0,0 +1,26 @@
+import { getNewsList } from "@/app/_libs/microcms";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import NewsList from "@/app/_components/NewsList";
+import SearchField from "@/app/_components/SearchField";
+import Breadcrumbs from "@/app/_components/Breadcrumbs";
+
+type Props = {
+  searchParams: {
+    q?: string;
+  };
+};
+
+export default async function Page({ searchParams }: Props) {
+  const { contents: news } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+    q: searchParams.q,
+  });
+
+  return (
+    <>
+      <Breadcrumbs />
+      <SearchField />
+      <NewsList news={news} />
+    </>
+  );
+}
