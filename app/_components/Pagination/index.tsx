f4270cf2716dedc81aea197951a02a0c1c2c9c8e
KazumaOchiai <kazuma06101209@gmail.com>
2025-08-31 16:42:58 +0900

    検索結果のページネーション完了

--- app/_components/Pagination/index.tsx
+++ app/_components/Pagination/index.tsx
@@ -6,12 +6,14 @@ type Props = {
   totalCount: number;
   current?: number;
   basePath?: string;
+  q?: string;
 };
 
 export default function Pagination({
   totalCount,
   current = 1,
   basePath = "/news",
+  q,
 }: Props) {
   const pages = Array.from(
     { length: Math.ceil(totalCount / NEWS_LIST_LIMIT) },
@@ -21,17 +23,27 @@ export default function Pagination({
   return (
     <nav>
       <ul className={styles.container}>
-        {pages.map((p) => (
-          <li className={styles.list} key={p}>
-            {current !== p ? (
-              <Link href={`${basePath}/p/${p}`} className={styles.item}>
-                {p}
-              </Link>
-            ) : (
-              <span className={`${styles.item} ${styles.current}`}>{p}</span>
-            )}
-          </li>
-        ))}
+        {pages.map((p) => {
+          let href;
+          if (basePath === "/news/search") {
+            href = q
+              ? `${basePath}?page=${p}&q=${encodeURIComponent(q)}`
+              : `${basePath}?page=${p}`;
+          } else {
+            href = `${basePath}/p/${p}`;
+          }
+          return (
+            <li className={styles.list} key={p}>
+              {current !== p ? (
+                <Link href={href} className={styles.item}>
+                  {p}
+                </Link>
+              ) : (
+                <span className={`${styles.item} ${styles.current}`}>{p}</span>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
--- app/news/search/page.tsx
+++ app/news/search/page.tsx
@@ -1,18 +1,24 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 import Breadcrumbs from "@/app/_components/Breadcrumbs";
 
 type Props = {
   searchParams: {
     q?: string;
+    page?: string;
   };
 };
 
 export default async function Page({ searchParams }: Props) {
-  const { contents: news } = await getNewsList({
+  const page = Number(searchParams.page) || 1;
+  const offset = (page - 1) * NEWS_LIST_LIMIT;
+
+  const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
+    offset, // ← 追加
     q: searchParams.q,
   });
 
@@ -21,6 +27,12 @@ export default async function Page({ searchParams }: Props) {
       <Breadcrumbs />
       <SearchField />
       <NewsList news={news} />
+      <Pagination
+        totalCount={totalCount}
+        q={searchParams.q}
+        current={page}
+        basePath="/news/search"
+      />
     </>
   );
 }
